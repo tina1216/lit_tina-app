6a44b6a2e76dd40437373e47314f8448f3c77944
tina1216 <takako.ballet.1216@gmail.com>
2019-05-23 11:51:49 +0000

    lots of commit

--- models.rb
+++ models.rb
@@ -2,7 +2,7 @@ require 'bundler/setup'
 Bundler.require
 
 # ActiveRecord::Base.establish_connection(ENV['DATABASE_URL']||"sqlite3:db/development.db")
-ActiveRecord::Base.establish_connection("sqlite3:db/development.db")
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL']||"sqlite3:db/development.db")
 
 class User < ActiveRecord::Base
   has_secure_password
